8e69c4082d9429ad27cd4b31ddc4dce0cff0ebb3
Luis cañas <luis.santos748@yahoo.com>
2018-01-29 09:43:15 -0600

    Revert "Revert "Revert "Ejercicios fin"""
    This reverts commit f6e87b7fb73fb9a76d2a1885b8e9fab3970b4421.

---
+++ src/main/java/com/sv/udb/clases/Ejercicio1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author luisedu
+ */
+public class Ejercicio1 {
+
+    List<Moneda> listMone;
+
+    public Ejercicio1() {
+        this.listMone = new ArrayList<>();
+    }
+
+    public void agregar(int cant, double deno) {
+        this.listMone.add(new Moneda(cant, deno));
+    }
+
+    public double calcular() {
+        double resp = 0;
+        try {
+            for (Moneda temp : this.listMone) {
+                resp += (temp.getCant() * temp.getDeno());
+            }
+        } catch (Exception ex) {
+            System.err.println("Error al calcular: " + ex.getMessage());
+        }
+        return resp;
+    }
+
+}
